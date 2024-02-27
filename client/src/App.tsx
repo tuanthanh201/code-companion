d9fb09c5e8e5465cf43f38d61701b3504c25e247
Thanh Nguyen <ngthanh2248@gmail.com>
2024-02-27 12:16:42 -0500

    Add theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,21 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import Hero from './components/Hero';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/authentication/Login';
+import Register from './components/authentication/Register';
+import theme from './theme';
+
 const App = () => {
 	return (
-		<ChakraProvider>
-			<Hero />
+		<ChakraProvider theme={theme}>
+			<Router>
+				<Routes>
+					<Route path='/' element={<Hero />} />
+					<Route path='/login' element={<Login />} />
+					<Route path='/register' element={<Register />} />
+				</Routes>
+			</Router>
 		</ChakraProvider>
 	);
 };
