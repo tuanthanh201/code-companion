cc0c683e0547ed1c8fe424783287b9809564178c
Semih Chelik <semihchelik34@gmail.com>
2024-03-31 21:08:56 -0400

    chat

--- client/src/components/chat/ChatContent.tsx
+++ client/src/components/chat/ChatContent.tsx
@@ -6,47 +6,91 @@ import {
 	MessageInput,
 	MessageList,
 } from '@chatscope/chat-ui-kit-react';
-import { v4 as uuidv4 } from 'uuid';
-import { pushChatMessage } from '../../store/chat/chatSlice';
-import { RootState, useAppDispatch, useAppSelector } from '../../store';
+import { ChatMessage } from '../../store/chat/chatSlice';
+import { m } from "framer-motion"
+import { useState } from "react"
+
+let localId = 1
 
 const ChatContent = () => {
-	const dispatch = useAppDispatch();
-	const { chat, currentUser } = useAppSelector(
-		(state: RootState) => state.chat
-	);
-	const currentChat = chat[currentUser];
-
-	return (
-		<ChatContainer>
-			<ConversationHeader>
-				<ConversationHeader.Back />
-				<Avatar name={currentUser} src={`/avatars/${currentUser}.svg`} />
-				<ConversationHeader.Content userName={currentUser} />
-			</ConversationHeader>
-			<MessageList>
-				{currentChat.messages.map((message) => (
-					<Message
-						key={message.id}
-						model={{ ...message, position: 'normal' }}
-					/>
-				))}
-			</MessageList>
-			<MessageInput
-				placeholder='Type message here'
-				onSend={(_, textContent) =>
-					dispatch(
-						pushChatMessage({
-							id: uuidv4(),
-							direction: 'outgoing',
-							message: textContent,
-							sender: 'User',
-						})
-					)
-				}
-			/>
-		</ChatContainer>
-	);
-};
+  const [messages, setMessages] = useState<ChatMessage[]>([
+    {
+      id: `${localId++}`,
+      direction: "incoming",
+      message: "Hello my friend",
+      sender: "Zoe",
+      sentTime: "15 mins ago"
+    },
+    {
+      id: `${localId++}`,
+      direction: "outgoing",
+      message: "Hello my friend",
+      sender: "Patrik",
+      sentTime: "15 mins ago"
+    }
+  ])
+  return (
+    <ChatContainer>
+      <ConversationHeader>
+        <ConversationHeader.Back />
+        <Avatar
+          name="Zoe"
+          src="https://chatscope.io/storybook/react/assets/zoe-E7ZdmXF0.svg"
+        />
+        <ConversationHeader.Content
+          // info='Active 10 mins ago'
+          userName="Zoe"
+        />
+        {/* <ConversationHeader.Actions>
+      <VoiceCallButton />
+      <VideoCallButton />
+      <EllipsisButton orientation='vertical' />
+    </ConversationHeader.Actions> */}
+      </ConversationHeader>
+      <MessageList
+        typingIndicator={<TypingIndicator content="Zoe is typing" />}
+      >
+        <MessageSeparator content="Saturday, 30 November 2019" />
+        {messages.map((message) => (
+          <Message
+            key={message.id}
+            model={{ ...message, position: "normal" }}
+            children={
+              message.sender === "Zoe" ? (
+                <Avatar
+                  src="https://chatscope.io/storybook/react/assets/zoe-E7ZdmXF0.svg"
+                  name="Zoe"
+                />
+              ) : (
+                <Avatar
+                  src={
+                    "https://chatscope.io/storybook/react/assets/emily-xzL8sDL2.svg"
+                  }
+                  name="Emily"
+                />
+              )
+            }
+          />
+        ))}
+      </MessageList>
+      <MessageInput
+        placeholder="Type message here"
+        onSend={(innerHtml, textContent, innerText, nodes) =>
+          //append the message to the messages list
+          setMessages([
+            ...messages,
+            {
+              id: `${localId++}`,
+              direction: "outgoing",
+              message: innerText,
+              sender: "Patrik",
+              sentTime: "15 mins ago"
+            }
+          ])
+        }
+      />
+    </ChatContainer>
+  )
+}
 
 export default ChatContent;
