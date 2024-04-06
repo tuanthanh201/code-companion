5411e35de27e6273cb4c771ea3c8b9b9cdb59a71
Thanh Nguyen <ngthanh2248@gmail.com>
2024-04-06 12:48:31 -0400

    Clean up

--- client/src/store/auth/authActions.ts
+++ client/src/store/auth/authActions.ts
@@ -13,7 +13,6 @@ export interface LoginInput {
 
 export const login = (params: LoginInput) => {
 	return async (dispatch: AppDispatch) => {
-		// TODO: Make an API request to login
 		// If successful, dispatch the save user action
 		dispatch(saveUserAction(params.username));
 	};
@@ -27,7 +26,6 @@ export const logout = () => {
 
 export const register = (params: RegisterInput) => {
 	return async (dispatch: AppDispatch) => {
-		// TODO: Make an API request to register
 		// If successful, dispatch the save user action
 		dispatch(saveUserAction(params.username));
 	};
